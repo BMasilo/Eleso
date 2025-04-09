06cfc7a7dffe42db731896d8e672f1c35ed85d10
BMasilo <40583969+BMasilo@users.noreply.github.com>
2025-04-09 09:02:35 +0200

    modified Social.js

--- eleso/src/App.js
+++ eleso/src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <Social/>
       <Main/>
-      <Social/>
     </div>
   );
 }
--- eleso/src/Components/Social.js
+++ eleso/src/Components/Social.js
@@ -1,14 +1,16 @@
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import {faInstagram, faWhatsapp, faXTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons'
 
 const Social = () => {
 	return (
         <div className="head-section">
-            <div><i className="fa fa-envelope">eleso@gmail.com</i></div>
+            <div><FontAwesomeIcon icon={faEnvelope}/>eleso@gmail.com</div>
             <div>
-                <i className="fa-regular fa-envelope"></i>
-                <i className="fa-brands fa-x-twitter"></i>
-                <i className="fa fa-envelope"></i>
-                <i className="fa fa-envelope"></i>
+            <FontAwesomeIcon icon={faXTwitter} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faYoutube} />
+            <FontAwesomeIcon icon={faWhatsapp} />
             </div>
         </div>
 	)
